e6dda18e854084e9546c6e06ab5e847309ddd06a
EllbellCode <EllbellCode@gmail.com>
2024-11-19 13:17:07 +0000

    Time: 19 ms (67.95%), Space: 12.5 MB (29.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -6,13 +6,16 @@ class Solution(object):
 
         for i in range(len(s)):
 
-            if s[i] in characters: #If s is already in the sequence, start again
+            if s[i] in characters: 
+                #If s[i] is already in the sequence, remove the current sequence up to and including the original s[i]
                 characters = characters[characters.index(s[i])+1:]
-            
-            characters.append(s[i])
+                characters.append(s[i])
 
-            if len(characters) > longest_length: #If the current sequence is longer than the current longest, replace
-                longest_length = len(characters)
+            else:
+                characters.append(s[i])
+                if len(characters) > longest_length:
+                     #If the current sequence is longer than the current longest, replace
+                    longest_length = len(characters)
         
         return longest_length
 

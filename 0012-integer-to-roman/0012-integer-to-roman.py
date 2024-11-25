5d732037d34cc3f9403ee44381e21ffaafd86691
EllbellCode <EllbellCode@gmail.com>
2024-11-25 14:16:28 +0000

    Time: 4 ms (80.81%), Space: 11.6 MB (60.56%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,12 +1,18 @@
 class Solution(object):
     def intToRoman(self, num):
-
+        
+        #Couples of numerals and their values
         roman_numerals = [
         ('M', 1000), ('CM', 900), ('D', 500), ('CD', 400),
         ('C', 100), ('XC', 90), ('L', 50), ('XL', 40),
         ('X', 10), ('IX', 9), ('V', 5), ('IV', 4), ('I', 1)
         ]
 
+        #Iterate through numbers and subtract from num until it is
+        #less than the numeral then move on to the next numeral
+
+        #This only works if num < 3999 as 4000 would be represented by I(10000 symbol)
+        #Which is not in our set
         result = []
         for roman, value in roman_numerals:
             while num >= value:

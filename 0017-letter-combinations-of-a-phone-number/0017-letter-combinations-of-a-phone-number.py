e52dfae5cd215f4b36ab7624fbfeb19b73bc2ff7
EllbellCode <EllbellCode@gmail.com>
2024-11-27 15:11:33 +0000

    Time: 0 ms (100%), Space: 12.2 MB (18.79%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+
+        if digits == "":
+            return []
+
+        import itertools
+
+        letters = {1: [''], 2: ['a','b','c'], 3: ['d','e','f'], 4: ['g','h','i'], 
+                    5: ['j','k','l'], 6: ['m','n','o'], 7: ['p','q','r', 's'], 
+                   8: ['t','u','v'], 9: ['w','x','y', 'z']}
+
+        digit_letters = []
+        combinations = []
+
+        for char in digits:
+            digit_letters.append(letters[int(char)])
+
+        for element in itertools.product(*digit_letters):
+            combinations.append(''.join(element))
+
+        return combinations

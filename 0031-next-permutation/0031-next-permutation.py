f5eec7dceb2b0593f411e37bf3a9aabb30b832bf
EllbellCode <EllbellCode@gmail.com>
2025-03-07 14:06:23 +0000

    Time: 0 ms (100%), Space: 12.5 MB (13.63%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,26 +4,37 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        #default pivot value out of range contraints
         pivot = -1
-        for i in range(len(nums) - 2, -1, -1):  # Traverse backwards
-            if nums[i] < nums[i + 1]:  # Find the first decreasing element
+
+        #find pivot value
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]: 
                 pivot = i
                 break
 
+        #if we still have default pivot value
+        #then the array is in largest lexicographic order
+        # so we reverse 
         if pivot == -1:
             nums.reverse()
             return nums
         
+        
+        # set default value to pivot if there is no element less than pivot
         swap_index = pivot
-        for i in range(len(nums) - 1, pivot, -1):  # Traverse from the end
-            if nums[i] > nums[pivot]:  # Find the smallest larger element
+
+        # find pivot value
+        for i in range(len(nums) - 1, pivot, -1):  
+            if nums[i] > nums[pivot]: 
                 swap_index = i
                 break
             
+        #swap pivot and swap around
         nums[pivot], nums[swap_index] = nums[swap_index], nums[pivot]
 
+        #two pointers to reverse right side of pivot
         left, right = pivot+1, len(nums)-1
-
         while left < right:
 
             nums[left], nums[right] = nums[right], nums[left]

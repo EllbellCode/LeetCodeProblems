0c689ee043489ad675e777d07e3ba1c5b900a012
EllbellCode <elliotaj7@yahoo.com>
2025-09-30 13:37:42 +0100

    Time: 0 ms (100%), Space: 12.5 MB (52.96%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+
+        int1 = int(num1)
+        int2 = int(num2)
+
+        prod = int1 * int2
+
+        return str(prod)
+    

4950b3b5559b049a52a1b8396b245e05812b21b0
EllbellCode <elliotaj7@yahoo.com>
2025-11-27 16:28:03 +0000

    Time: 0 ms (100%), Space: 12.5 MB (59.77%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -8,6 +8,37 @@ class Solution(object):
 
         # We are looking to calculate x**n
         # Naive approach is to simply return x**n
+        # This will actually work as pythons power operation is optimised
+        # So assume we cannot do this
+        # The best way is modular exponentiati
+        
+        # If n is even, set x = x*x and divide n by 2
+        # If n is odd, subtract 1 from n, multiply a counter by x then repeat n even step
+        
+        remainder = 1
+        neg = False
 
-        return x**n
+        if n == 0:
+            return 1
+
+        if n < 0:
+            neg = True
+            n = abs(n)
+
+        while n > 1:
+
+            if n % 2 == 0:
+
+                x = x*x
+                n = n//2
+
+            elif n % 2 == 1:
+
+                remainder = remainder*x
+                n = n - 1
+
+        if neg:
+            return 1/(x*remainder)
+        else:
+            return x*remainder
         

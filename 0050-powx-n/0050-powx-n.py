5c2a5e84f4403a6f49300ab72a38f5f1b8e4b454
EllbellCode <elliotaj7@yahoo.com>
2025-11-27 16:06:57 +0000

    Time: 0 ms (100%), Space: 12.4 MB (59.77%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+
+        # We are looking to calculate x**n
+        # Naive approach is to simply return x**n
+
+        return x**n
+        
